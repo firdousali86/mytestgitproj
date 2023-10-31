2246d527507d4ff33b4adfddd29b896872d88c01
Firdous Ali <firdous.ali.tp@gmail.com>
2023-10-31 14:41:18 +0500

    my new feature 1

---
+++ myfeature1.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function myfeature1() {
+  return (
+    <View>
+      <Text>myfeature1</Text>
+    </View>
+  );
+}
